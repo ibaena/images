95746a3c2707f9562b9310950e9bc6aa0f6395e1
Ivan <ivan.baena04@gmail.com>
2017-04-25 16:05:26 -0400

    props

---
+++ client/components/image_detail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ImageDetail = (props) => {
+  //props.image => this is the image object
+  //props.image.title
+  //props.image.link
+
+  return (
+    <li>
+      <img src={props.image.link} />
+      {props.image.title}
+    </li>
+  );
+};
+
+export default ImageDetail;
--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,13 +1,23 @@
 // Create image list component
-
 // Import React
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  {title:'Pen', link:'https://dummyimage.com/600x400'},
+  {title:'Pine Tree', link:'https://dummyimage.com/600x400'},
+  {title:'Mug', link:'https://dummyimage.com/600x400'},
+]
 
 // Create our component
 const ImageList = () => {
+  const RenderedImages = IMAGES.map( (image) => {
+    return <ImageDetail image={image} />
+  });
+
   return (
     <ul>
-      <li>Image Detail</li>
+      {RenderedImages}
     </ul>
   );
 };
