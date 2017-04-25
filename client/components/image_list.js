7934d2bbaaf9b8bcd5e01387449b2bc9b60a6ea3
Ivan <ivan.baena04@gmail.com>
2017-04-25 15:42:29 -0400

    nesting

---
+++ client/components/image_list.js
@@ -0,0 +1,16 @@
+// Create image list component
+
+// Import React
+import React from 'react';
+
+// Create our component
+const ImageList = () => {
+  return (
+    <ul>
+      <li>Image Detail</li>
+    </ul>
+  );
+};
+
+// Export our component
+export default ImageList;
